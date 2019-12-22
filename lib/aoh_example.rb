7bab10a20e6a44cee0cb5c56d6efdd232b71020f
Adam  Shappy <ajs11300@gmail.com>
2019-12-22 14:48:11 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,7 +18,10 @@ def literal_aoh
   aoh = [
     { name:  "Don Gately", occupation: "Live-in Staff" }, 
     { name:  "Joelle van Dyne", occupation: "Radio Personality" }, 
-    { name:  "Pat Monteseian", occupation: "Staff" }, {}, {}]
+    { name:  "Pat Monteseian", occupation: "Staff" }, 
+    { name:  "Kate Gompert", occupation: "None" }, 
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
+    ]
 end
 
 def aoh_lookup(aoh, row, key)
